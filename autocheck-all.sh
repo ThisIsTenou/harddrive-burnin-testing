7f7fbaa26d6d353277d9c5d63361e4818558272b
Leo <57889616+ThisIsTenou@users.noreply.github.com>
2021-12-29 21:19:06 +0100

    Update autocheck-all.sh

--- autocheck-all.sh
+++ autocheck-all.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+# Check if user is root or using sudo
+if [ "$USER" != root ] && [ "$SUDO_USER" != root ]; then
+	echo "This script needs to be executed as root or with sudo!"
+	exit 1
+fi
+
+# Define variables
+fileloc="testresults/"
+fileloc="${fileloc%/}"
+
 # Find all harddrives capable of SMART-Testing
 readarray -t devices < <( smartctl --scan | cut -d\# -f1 )
-mkdir -p "testresults"
+mkdir -p "$fileloc/"
 
 for devicesmart in "${devices[@]}"; do
     device="$(echo $devicesmart | cut -d " " -f1)"
     if [[ $(smartctl $devicesmart -a | grep 'Rotation Rate' | grep 'rpm') ]]; then
         echo "Starting tests for $device"
-        ./check-disk.sh "$device" "testresults/" &
+        ./check-disk.sh "$device" "$fileloc" &> output.log &
     else
         echo "$device is not a HDD, skipped."
         devices=( "${devices[@]/$device}" )
